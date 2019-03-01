2f4637d4327582cc818d40feec5f3ccb79c50a55
Weronika Chodanionek <w.chodanionek@grupazero.pl>
2019-03-01 15:10:50 +0100

    add script

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  //scrollowanie na następny background-image
+  $('a[href^="#"]').on("click", function(event) {
+    var target = $($(this).attr("href"));
+
+    if (target.length) {
+      event.preventDefault();
+      $("html, body").animate(
+        {
+          scrollTop: target.offset().top
+        },
+        1000
+      );
+    }
+  });
+});
