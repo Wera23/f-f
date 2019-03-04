e28ca4b070abd5ebb185593fdbdd6123b95b2e80
Weronika Chodanionek <w.chodanionek@grupazero.pl>
2019-03-04 15:06:07 +0100

    fix styles

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
-  //scrollowanie na następny background-image
-  $('a[href^="#"]').on("click", function(event) {
-    var target = $($(this).attr("href"));
+  // scrollowanie na następny background-image
+  $('a[href^="#"]').on('click', function(event) {
+    const target = $($(this).attr('href'))
 
     if (target.length) {
-      event.preventDefault();
-      $("html, body").animate(
+      event.preventDefault()
+      $('html, body').animate(
         {
           scrollTop: target.offset().top
         },
         1000
-      );
+      )
     }
-  });
-});
+  })
+})
